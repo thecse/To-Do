2a96e525c652ccd140357bfe78fb8c0b5972d390
Rohan Mehta <bestofrkm@gmail.com>
2024-10-11 05:58:32 +0530

    Updates

--- src/constant/FilterConstant.tsx
+++
@@ -1,5 +0,0 @@
-export const FilterBy = {
-    all: "All",
-    completed: "Completed",
-    incompleted: "Incompleted"
-}
--- src/constant/KeyConstants.tsx
+++
@@ -1,2 +0,0 @@
-export const TO_DO_KEY_SUFFIX = "to-do"
-export const FILTER_KEY = "filter"
